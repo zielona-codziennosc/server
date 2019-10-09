a3ffc7f3a40b5df58f42a17cce0d1e5c4680e7ef
antekpapieski <maciejpapiewski9@gmail.com>
2019-10-09 21:51:35 +0200

    added GUS tests

---
+++ tests/gusDrivers/locationRelated.js
@@ -0,0 +1,35 @@
+import chai, {expect} from 'chai';
+import chaiHttp  from 'chai-http'
+import server from "../../index";
+import jwt from "jsonwebtoken";
+
+import getMatchingUnitsFromCoordinates from "../../gusDrivers/getMatchingUnitsFromCoordinates";
+
+
+let should = chai.should();
+chai.use(chaiHttp);
+
+
+describe('GUS location and unit-id related functions', function() {
+
+    describe("The getMatchingUnitsFromCoordinates function responsible for the initial communication", () => {
+
+        it("Should return 2 matching units: Warsaw powiat and Mazowieckie voivodeship", done => {
+            const warsawCoordinates = "52.231246, 21.004800";
+
+            getMatchingUnitsFromCoordinates(warsawCoordinates).then( matchingUnits => {
+
+                expect(matchingUnits).to.be.an("array");
+                expect(matchingUnits).to.have.lengthOf(2);
+
+                const warsawPowiat = matchingUnits.find(unit => unit.name === "Powiat m. st. Warszawa");
+                const mazowieckieVoivodeship = matchingUnits.find(unit => unit.name === "MAZOWIECKIE");
+
+                expect(warsawPowiat).to.exist;
+                expect(mazowieckieVoivodeship).to.exist;
+
+                done();
+            })
+        })
+    })
+});
