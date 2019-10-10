561635746d11c9a96cf52a0442b871cad6c43251
antekpapieski <maciejpapiewski9@gmail.com>
2019-10-10 17:58:10 +0200

    addition of gusUnitId to user model

--- models/user.js
+++ models/user.js
@@ -3,12 +3,13 @@ import { Schema, model } from 'mongoose';
 import { ensureFieldUniquity, accountOfEmail } from "./utils";
 
 const UserSchema = new Schema({
-    email: { type: String, required: true, unique: true}
-}, {strict: false, minimize: false});
+    email: { type: String, required: true, unique: true},
+    gusUnitId: { type: String, default: "000000000000" } //the default is whole Poland
+});
 
 
 UserSchema.pre(['save', 'findOneAndUpdate', 'findByIdAndUpdate'], ensureFieldUniquity("email"));
 
 UserSchema.static("accountOfEmail", accountOfEmail);
 
-export default model('User', UserSchema)
+export default model('User', UserSchema);
